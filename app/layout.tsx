c5d4d1e6255bbf653e301784224fdfd6ecc80507
clieantapp-crypto <clieantapp@gmail.com>
2025-12-15 09:08:26 +0300

    fdg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from '@/context/AuthContext';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -31,7 +32,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>{children}</AuthProvider></body>
     </html>
   );
 }
